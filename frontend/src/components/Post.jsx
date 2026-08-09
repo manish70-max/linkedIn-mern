7d872af8b0a4313a27202b3dee993574fa74381c
manish70-max <manishvarma7043@gmail.com>
2026-08-09 16:14:48 +0530

    Update Post.jsx

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -11,7 +11,7 @@ import { LuSendHorizontal } from "react-icons/lu";
 import {io} from "socket.io-client"
 import ConnectionButton from './ConnectionButton';
 
-let socket=io("http://localhost:8000")
+let socket=io("https://linkedin-backend-hmp7.onrender.com")
 function Post({ id, author, like, comment, description, image,createdAt }) {
     
     let [more,setMore]=useState(false)
