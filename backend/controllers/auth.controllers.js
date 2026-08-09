52d946db07c198d92515c1f1bf0fc3cd6bab8685
manish70-max <manishvarma7043@gmail.com>
2026-08-09 14:49:02 +0530

    Update auth.controllers.js

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -30,7 +30,7 @@ export const signUp=async (req,res)=>{
        res.cookie("token",token,{
         httpOnly:true,
         maxAge:7*24*60*60*1000,
-        sameSite:"none",
+        sameSite:"None",
         secure:true
        })
       return res.status(201).json(user)
@@ -59,7 +59,7 @@ export const login=async (req,res)=>{
         res.cookie("token",token,{
          httpOnly:true,
          maxAge:7*24*60*60*1000,
-         sameSite:"none",
+         sameSite:"None",
          secure:true
         })
        return res.status(200).json(user)
