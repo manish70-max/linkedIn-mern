b748271a7ddaf35bfe2181590cd5fed2c74df6d0
manish70-max <manishvarma7043@gmail.com>
2026-08-09 15:14:53 +0530

    Update Network.jsx

--- frontend/src/pages/Network.jsx
+++ frontend/src/pages/Network.jsx
@@ -8,7 +8,8 @@ import { RxCrossCircled } from "react-icons/rx";
 
 import io from "socket.io-client"
 
-const socket=io("https://linkedin-max.onrender.com")
+
+const socket = io("https://linkedin-max.onrender.com");
 function Network() {
 let {serverUrl}=useContext(authDataContext)
 let [connections,setConnections]=useState([])
