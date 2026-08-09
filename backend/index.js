7b88fb902554c1c448522f64c969b9a4954fa45e
manish70-max <manishvarma7043@gmail.com>
2026-08-09 14:43:44 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ const server = http.createServer(app);
 
 export const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://linkedin-gjvg.onrender.com",
     credentials: true,
   },
 });
